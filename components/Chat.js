592dc9dd2d276c89be67744b5a07d210dd61f8fe
wentdavid <davidwent@me.com>
2023-02-17 16:33:42 +0100

    show Name as Title

--- components/Chat.js
+++ components/Chat.js
@@ -4,8 +4,8 @@ import { View, Text, Button,TextInput,StyleSheet } from "react-native";
 export default class Chat extends React.Component {
   render() {
     let color = this.props.route.params.color;
-    /* let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name }); */
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
 
     return (
       <View style={[styles.container, { backgroundColor: color }]}>
