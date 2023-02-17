5aece064a19a867f17962e8bfc1a1b97a6d6cb10
wentdavid <davidwent@me.com>
2023-02-17 14:14:56 +0100

    Add choosen background color

--- components/Chat.js
+++ components/Chat.js
@@ -3,11 +3,12 @@ import { View, Text, Button,TextInput,StyleSheet } from "react-native";
 
 export default class Chat extends React.Component {
   render() {
+    let color = this.props.route.params.color;
     /* let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name }); */
 
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={[styles.container, { backgroundColor: color }]}>
         <Button
           title="Go to Start"
           onPress={() => this.props.navigation.navigate("Start")}
@@ -16,3 +17,10 @@ export default class Chat extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+    
