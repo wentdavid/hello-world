59332c9549437b53277f1bb44d1a7773b5fa3e93
wentdavid <davidwent@me.com>
2023-02-21 19:13:02 +0100

    innit

---
+++ components/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Start from "./Start";
+import Chat from "./Chat";
+
+const Stack = createStackNavigator();
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
