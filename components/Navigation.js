ba39e0ed37ddbccfa9ea6f97edcb8b7877592322
wentdavid <davidwent@me.com>
2023-02-22 13:25:17 +0100

    Switch from Tab to Stack.Navigator

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,28 +1,21 @@
 import React from "react";
 import "react-native-gesture-handler";
+
 import { NavigationContainer } from "@react-navigation/native";
-/* import { createStackNavigator } from "@react-navigation/stack";*/
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import Start from "./Start";
 import Chat from "./Chat";
 
-const Tab = createBottomTabNavigator();
-/* const Stack = createStackNavigator();
- */
-export default class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Stack = createStackNavigator();
 
-  render() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="Start">
-          <Tab.Screen name="Start" component={Start} />
-          <Tab.Screen name="Chat" component={Chat} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat" component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
